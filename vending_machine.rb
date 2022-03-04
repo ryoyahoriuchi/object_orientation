3e080608fc5188ecafe025bf3f48bdf90080748b
fumtas1k <fumta01a7@gmail.com>
2022-03-04 14:21:00 +0900

    fix:メソッドの使い方を修正

--- vending_machine.rb
+++ vending_machine.rb
@@ -3,11 +3,11 @@ require_relative "./juice_manager.rb"
 require_relative "./cash.rb"
 
 class VendingMachine
-  def self.start
-    @cash = Cash.new
-    @juice_manager = JuiceManager.new
-    @accountant = Accountant.new(@cash, @juice_manager)
+  @cash = Cash.new
+  @juice_manager = JuiceManager.new
+  @accountant = Accountant.new(@cash, @juice_manager)
 
+  def self.start
     while true
       puts "あなたは何をするか決めてください"
       puts "1:購入、2:管理業務"
@@ -22,7 +22,7 @@ class VendingMachine
       else
         puts "1か2を選択下さい"
       end
-    end  
+    end
   end
 
   def self.choice
@@ -30,7 +30,7 @@ class VendingMachine
       puts "何をしますか？"
       puts "1:お金を投入、2:払い戻し、3:購入する"
       puts "購入可能リスト"
-      p @accountant.purchasable_list(@cash.amount_money).map{|i| i = i.to_s}
+      p @accountant.purchasable_list(@accountant.amount_money).map{|i| i = i.to_s}
       action = gets.chomp.to_s
       ["1", "2", "3"].include?(action)
       if action == "1"
@@ -68,31 +68,30 @@ class VendingMachine
     puts "対応可能硬貨： #{Cash::MONEY}"
     money = gets.to_i
       @accountant.insert_money(money)
-      puts @cash.amount_money
+      puts @accountant.amount_money
   end
 
   def self.refund
-    puts "#{@cash.amount_money}円を返却します。"
-    @cash.amount_money = 0
+    puts "#{@accountant.refund_money}円を返却します。"
   end
 
   def self.purchase
     puts "購入可能リストは下記の通りです。"
-    p @accountant.purchasable_list(@cash.amount_money).map{|i| i = i.to_s}
+    p @accountant.purchasable_list(@accountant.amount_money).map{|i| i = i.to_s}
     puts "何を購入しますか？"
     juice = gets.chomp
     if @accountant.purchasable?(juice)
-      @accountant.purchase(juice)
+      change = @accountant.purchase(juice) #購入操作は同時に返金も行われる
       puts "#{juice}を購入しました"
-      refund
-      puts @cash.amount_money #おつりが出力されない
-      puts @cash.sale_amount
+      puts "お釣りは#{change}"
+      puts "売上高は計#{@accountant.sale_amount}"
     else
       puts "購入できません"
       choice
     end
   end
 
+  def self.sale_amount
+    @accountant.sale_amount
+  end
 end
-
-VendingMachine.start
