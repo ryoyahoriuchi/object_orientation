860da4e9a9155610d1212084f244ed8bf95b7188
Miyauchi <hy.miyauchi@gmail.com>
2022-03-12 23:34:37 +0900

    a[fix]修正

--- vm_interface.rb
+++ vm_interface.rb
@@ -1,148 +1,138 @@
-require_relative "accountant"
-require_relative "cash"
-require_relative "juice_manager"
+require_relative "./accountant.rb"
+require_relative "./juice_manager.rb"
+require_relative "./cash.rb"
 
 class VendingMachine
   attr_reader :accountant, :juice_manager
+
   def initialize
-    cash = Cash.new
+    @cash = Cash.new
     @juice_manager = JuiceManager.new
-    @accountant = Accountant.new(cash, @juice_manager)
+    @accountant = Accountant.new(@cash, @juice_manager)
   end
 
   def start
-    role = choose(:role)
-    if role == 1 # 購入者
-      purchase_step
-    elsif role == 2 # 管理者
-      management_step
-    end
-  end
-
-  private
-
-  # 引数actionに応じて、メッセージと選択リスト、そしてリスト出力時にNo.を付けるかどうか判定するフラグの３つの要素の配列を返す
-  def get_message(action)
-    no_number = false
-    case action
-    when :role
-      list = %w[購入者 管理者]
-      message = "該当する数字を入力して下さい。"
-    when :action
-      list = %w[お金を投入 払い戻し 購入する]
-      message = "何をしますか?下記より数字を選択して下さい。"
-    when :juice
-      list = @accountant.purchasable_list(@accountant.amount_money).map(&:to_s)
-      message = "購入可能ジュースは下記の通りです。\n数字を選択して下さい。"
-    when :insert
-      list = Cash::MONEY
-      message = "使用できるお金は以下の通りです。\nお金を入力して下さい"
-      no_number = true
-    when :management
-      list = %w[商品追加 終了]
-      message = "何をしますか?下記より数字を選択して下さい。"
-    end
-    [list, message, no_number]
-  end
-
-  def get_input(list, message, no_number)
-    puts message
-    list_message =
-      if no_number
-        list.join(", ")
-      else
-        list.map.with_index{|select, i| "#{i+1}: #{select}"}.join(", ")
-      end
-    puts list_message
-    gets.chomp.tr("１-１０００", "1-1000")
-  end
-
-  # 引数actinによって、選択するリスト
-  def choose(action)
-    list, message, no_number = *get_message(action)
-    if Array(list).any?
-      num = get_input(list, message, no_number)
-      list_range =
-        if no_number
-          list.map(&:to_s)
-        else
-          (1..list.length).to_a.map(&:to_s)
-        end
-      while num
-        if list_range.include?(num)
-          return num.to_i
-        else
-          num = get_input(list, message, no_number)
-        end
-      end
-    end
-  end
-
-  def purchase_step
-    action = choose(:action)
     while true
-      case action
-      when 1
-        money = choose(:insert)
-        money = @accountant.insert_money(money)
-        if money
-          puts "#{money}は使用できるお金ではありません。"
+      puts "あなたは何をするか決めてください"
+      puts "1:購入、2:管理業務"
+      action = gets.chomp
+      if ["1", "2"].include?(action)
+        if action == "1"
+          choice
         else
-          puts "投入金額の合計は¥#{@accountant.amount_money}です"
+          management
         end
-        action = choose(:action)
-      when 2
-        puts "払い戻し金額は¥#{@accountant.refund_money}"
-        puts "ご利用ありがとうございました。"
-        break
-      when 3
-        list = @accountant.purchasable_list(@accountant.amount_money)
-        if list.any?
-          juice_num = choose(:juice)
-          juice = list[juice_num - 1]
-          change = @accountant.purchase(juice)
-          puts "#{juice}をご購入されました。"
-          puts "お釣りは¥#{change}です。"
-          puts "ありがとうございました。"
-          break
-        else
-          puts "購入できるジュースがありません。"
-          action = choose(:action)
+        return
+      else
+        puts "1か2を選択下さい"
+      end
+    end  
+  end
+
+  # 購入ターム
+
+  def choice
+    while true
+      puts "何をしますか？"
+      puts "1:お金を投入、2:払い戻し、3:購入する"
+      puts "購入可能リスト"
+      p @accountant.purchasable_list(@accountant.amount_money).map{|i| i = i.to_s}
+      action = gets.chomp
+      if ["1", "2", "3"].include?(action)
+        if action == "1"
+          insert
+        elsif action == "2"
+          puts "#{@accountant.amount_money}円を返却します。"
+          @accountant.refund_money
+          exit
+        elsif action == "3"
+          purchase
         end
+      else
+        puts "1～3を選択ください"
       end
     end
   end
 
+  def insert
+    puts "投入金額を決めてください。"
+    puts "対応可能硬貨： #{Cash::MONEY}"
+    money = gets.chomp
+    if @accountant.insert_money(money).nil?
+      puts "#{money}円の投入がありました。"
+      self.choice
+    else
+      puts "不正投入がありました。"
+      self.insert
+    end
+  end
 
-  def management_step
-    puts "【在庫】#{@juice_manager.stock_all}"
-    action = choose(:management)
-    case action
-    when 1
-      puts "何のジュースを追加しますか？"
-      juice = gets.chomp
-      unless @juice_manager.exist?(juice)
-        while true
-          puts "何円に設定しますか？"
-          price = gets.chomp
-          break if (price =~ /\A[1-9][0-9]*\z/)
-        end
+  def purchase
+    puts "購入可能リストは下記の通りです。"
+    p @accountant.purchasable_list(@accountant.amount_money).map{|i| i = i.to_s}
+    puts "何を購入しますか？"
+    juice = gets.chomp
+    if @accountant.purchasable?(juice)
+      change = @accountant.purchase(juice)
+      puts "#{juice}を購入しました"
+      puts "おつりは#{change}円です"
+    else
+      puts "購入できません"
+      choice
+    end
+  end
+
+  # 管理業務ターム
+
+  def management
+    while true
+      puts "何をしますか？"
+      puts "1:商品追加、2:商品リスト表示、3:終了"
+      action = gets.chomp.to_s
+      ["1", "2", "3"].include?(action)
+      if action == "1"
+        addition
+      elsif action == "2"
+        puts @juice_manager.stock_all
+      elsif action == "3"
+        puts "終了します。"
+        exit
+      else
+        puts "1～3を選択ください"
       end
+    end
+  end
+
+  def addition
+    puts "何のジュースを追加しますか？"
+    juice = gets.chomp
+    unless @juice_manager.exist?(juice)
       while true
-        puts "何本格納しますか?"
-        stock = gets.chomp
-        break if (stock =~ /\A[1-9][0-9]*\z/)
+        puts "何円に設定しますか？"
+        price = gets.chomp
+        break if (price =~ /\A[1-9][0-9]*\z/)
+        puts "不正な値です。数値で入力ください"
       end
-      @juice_manager.store(juice,price,stock)
-      self.management_step
-    when 2
-      puts "管理業務を終了しました"
+    end
+    while true
+      puts "何本格納しますか?"
+      stock = gets.chomp
+      break if (stock =~ /\A[1-9][0-9]*\z/)
+      puts "不正な値です。数値で入力ください"
+    end
+    @juice_manager.store(juice, price, stock)
+    if @juice_manager.exist?(juice)
+      price = @juice_manager.price(juice)
+      puts "#{juice}を#{price}円で#{stock}本追加しました"
+    else
+      puts "#{juice}を#{price}円で#{stock}本追加しました"
     end
   end
+
 end
 
 
-if __FILE__ == $0
-  vm = VendingMachine.new
-  vm.start
-end
+  # if __FILE__ == $0
+  #   vm = VendingMachine.new 
+  #   vm.start
+  # end
